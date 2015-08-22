7c1691c86868a72f5773d7fc31f76e2fe15e0122
Daniel <isc.daniel.brambila@gmail.com>
2015-08-22 01:41:09 -0500

    Init Ember

--- app/router.js
+++ app/router.js
@@ -6,6 +6,27 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('users', function() {
+    this.route('show');
+    this.route('edit');
+    this.route('add');
+  });
+
+  this.route('diets', function() {
+    this.route('show');
+    this.route('add');
+    this.route('edit');
+  });
+  this.route('groups', function() {
+    this.route('add');
+    this.route('show');
+    this.route('edit');
+  });
+  this.route('foods', function() {
+    this.route('add');
+    this.route('show');
+    this.route('edit');
+  });
 });
 
 export default Router;
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+     'ember-cli-bootswatch': 'cerulean'
     // Add options here
   });
 
